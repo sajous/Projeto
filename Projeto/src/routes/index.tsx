feb9a899921e552ce8f60ed988cdb3bd5b339457
Patrick Sajous <patricksajous17@gmail.com>
2022-09-10 12:49:11 -0300

    feat: Add React-Router-Dom

--- Projeto/src/routes/index.tsx
+++ Projeto/src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<p>Pagina Inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+    )
+}
